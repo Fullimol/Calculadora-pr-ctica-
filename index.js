069e63ae7d4a860a33db92a5d6ab9bbb50d0cb4b
Bruno Freijomil <bruno_freijomil@hotmail.com>
2022-09-01 19:52:32 -0300

    Realizando el JS de la calculadora

---
+++ index.js
@@ -0,0 +1,57 @@
+function init(){
+    //variables
+    var resultado = document.getElementById("resultado");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var division = document.getElementById("division");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var resta = document.getElementById("resta");
+    var igual = document.getElementById("igual");
+    var reset = document.getElementById("reset");
+    var cero = document.getElementById("cero");
+    var suma = document.getElementById("suma");
+
+    //eventos
+    siete.onclick = function(e){
+        resultado.textContent = resultado.textContent + "7"
+    }
+    ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent + "8"
+    }
+    nueve.onclick = function(e){
+        resultado.textContent = resultado.textContent + "9"
+    }
+    cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent + "4"
+    }
+    cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent + "5"
+    }
+    seis.onclick = function(e){
+        resultado.textContent = resultado.textContent + "6"
+    }
+    uno.onclick = function(e){
+        resultado.textContent = resultado.textContent + "1"
+    }
+    dos.onclick = function(e){
+        resultado.textContent = resultado.textContent + "2"
+    }
+    tres.onclick = function(e){
+        resultado.textContent = resultado.textContent + "3"
+    }
+    cero.onclick = function(e){
+        resultado.textContent = resultado.textContent + "0"
+    }
+
+    ////seguir con el botón de resetear
+
+
+}
+
